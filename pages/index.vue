57201a8113bf7b4407c1409b6c958f30023f2a96
kasprzyk <piotkas1@gmail.com>
2020-06-02 15:15:39 +0200

    head() added

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <h1>TEST</h1>
+    <h1>List of events</h1>
   </div>
 </template>
+<script>
+export default {
+  head() {
+    // <-- property used by vue-meta to add header tags
+    return {
+      title: 'Event Listing - Real World Events', // <-- For our title tag
+      meta: [
+        {
+          hid: 'description',
+          name: 'description', // <-- for our meta description tag
+          content:
+            'Where you can find all the events taking place in your neighborhood'
+        }
+      ]
+    }
+  }
+}
+</script>
