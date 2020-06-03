a0a2cca9715a2f21d87097fcb91f9a1edb766e12
kasprzyk <piotkas1@gmail.com>
2020-06-03 14:25:11 +0200

    API Calls

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,39 @@
 <template>
   <div>
-    <h1>List of events</h1>
+    <h1>Events</h1>
+
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
-  head() {
-    // <-- property used by vue-meta to add header tags
-    return {
-      title: 'Event Listing - Real World Events', // <-- For our title tag
-      meta: [
-        {
-          hid: 'description',
-          name: 'description', // <-- for our meta description tag
-          content:
-            'Where you can find all the events taking place in your neighborhood'
+  components: {
+    EventCard
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        return {
+          events: response.data
         }
-      ]
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again.'
+        })
+      })
+  },
+  head() {
+    return {
+      title: 'Event Listing'
     }
   }
 }
