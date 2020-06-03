c05c074a0f43ccc01cb17f07d3bd1d04b7102e0f
kasprzyk <piotkas1@gmail.com>
2020-06-03 13:29:06 +0200

    Changes in _id

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    <h1>Individual Event: {{ this.$route.params.id }}</h1>
+    <h1>Event #{{ id }}</h1>
   </div>
 </template>
+<script>
+export default {
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  },
+  head() {
+    return {
+      title: 'Event #' + this.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'What you need to know about Event #' + this.id
+        }
+      ]
+    }
+  }
+}
+</script>
